1368747379d0de38620f782d8fa8530e4dc88683
sande <sandeepvkale@gmail.com>
2026-07-16 23:25:08 +0200

    Lambda Expression with Functional Interface with a method with one argument.

--- src/com/svk/java8/lambda/CalculatorImpl.java
+++ src/com/svk/java8/lambda/CalculatorImpl.java
@@ -1,16 +1,21 @@
 package com.svk.java8.lambda;
 
+@FunctionalInterface
 interface Calculator{
-    void switchOn();
+    //void switchOn();
+
+    void sum(int input);
         }
 
 public class CalculatorImpl {
 
     public static void main(String[] args) {
 
-    Calculator calculator = () -> System.out.println(" Switch On");
+        //Calculator calculator = () -> System.out.println(" Switch On");
+        Calculator calculator = (input) -> System.out.println(" Input : " + input);
 
-    calculator.switchOn();
+        //calculator.switchOn();
+        calculator.sum(12345);
 
     }
 }
