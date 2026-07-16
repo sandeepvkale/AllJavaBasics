dfd00a9e67ede6b31f106aea4f670aa195ab383e
sande <sandeepvkale@gmail.com>
2026-07-16 23:46:05 +0200

    Lambda Expression with Functional Interface with a method with return type and two argument.

--- src/com/svk/java8/lambda/CalculatorImpl.java
+++ src/com/svk/java8/lambda/CalculatorImpl.java
@@ -2,9 +2,11 @@ package com.svk.java8.lambda;
 
 @FunctionalInterface
 interface Calculator{
-    //void switchOn();
+    //void switchOn(); Method without param
 
-    void sum(int input);
+//    void sum(int input); Method with One param
+
+    int subStract(int i1, int i2); // Method with return type and 2 params
         }
 
 public class CalculatorImpl {
@@ -12,10 +14,22 @@ public class CalculatorImpl {
     public static void main(String[] args) {
 
         //Calculator calculator = () -> System.out.println(" Switch On");
-        Calculator calculator = (input) -> System.out.println(" Input : " + input);
-
         //calculator.switchOn();
-        calculator.sum(12345);
+
+        // Method with One param
+//        Calculator calculator = (input) -> System.out.println(" Input : " + input);
+//        calculator.sum(12345);
+
+        Calculator calculator = (i1, i2) ->
+        {
+            if(i1>i2){
+                throw new RuntimeException("Some Error");
+            } else {
+               return i2 - i1;
+            }
+        };
+
+                System.out.println(" Substract Value : " + calculator.subStract(8, 20));
 
     }
 }
