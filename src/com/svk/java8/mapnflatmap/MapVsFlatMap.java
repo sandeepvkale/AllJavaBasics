effc46818d2520974d4cbaf481e2a4c1237de3b8
sande <sandeepvkale@gmail.com>
2026-08-09 21:25:12 +0200

    Map Vs Flat Map Complete.

--- src/com/svk/java8/mapnflatmap/MapVsFlatMap.java
+++ src/com/svk/java8/mapnflatmap/MapVsFlatMap.java
@@ -4,14 +4,21 @@ import com.svk.java8.data.Customer;
 import com.svk.java8.data.CustomerDB;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MapVsFlatMap {
     static void main(String[] args) {
-    List<Customer> customers = CustomerDB.getAll();
+        List<Customer> customers = CustomerDB.getAll();
 
-    List<String> emails = customers.stream().map(customer -> customer.getEmail()).collect(Collectors.toList());
+        List<String> emails = customers.stream().map(customer -> customer.getEmail()).collect(Collectors.toList());
 
         System.out.println("List of Emails: " + emails);
+
+        List<List<String>> phoneNumbers = customers.stream().map(customer -> customer.getPhoneNumbers()).collect(Collectors.toList());
+        System.out.println("List of Phone Numbers: " + phoneNumbers);
+
+        List<String> phoneList = customers.stream().flatMap(customer -> customer.getPhoneNumbers().stream()).collect(Collectors.toList());
+        System.out.println("List of Phone Numbers in list using FlatMap: " + phoneList);
     }
 }
